5500c08282617eb1a9610a00cd1ed46d6424bf04
Yinshangyi <jl.dejaeger@gmail.com>
2022-08-13 16:35:01 +0200

    Reverse linked list (two functions)

--- src/linked_list/reverse.py
+++ src/linked_list/reverse.py
@@ -1,5 +1,27 @@
-from src.linked_list.linked_list import LinkedList
+from src.linked_list.linked_list import Node
 
 
-def reverse_linked_list(linked_list: LinkedList) -> LinkedList:
+def reverse_linked_list(head_node: Node) -> Node:
+    prev_node = None
+    current_node = head_node
+    while current_node is not None:
+        next_node = current_node.next
+        current_node.next = prev_node
+        prev_node = current_node
+        current_node = next_node
+    return prev_node
 
+
+def reverse_linked_list_rec(head_node: Node) -> Node:
+    # If head is empty or has reached the list end
+    if head_node is None or head_node.next is None:
+        return head_node
+    # Reverse the rest list
+    rest = reverse_linked_list_rec(head_node.next)
+
+    # Put first element at the end
+    head_node.next.next = head_node
+    head_node.next = None
+
+    # Fix the header pointer
+    return rest
--- test/linked_list/test_reverse.py
+++ test/linked_list/test_reverse.py
@@ -1,11 +1,24 @@
 from src.linked_list.linked_list import LinkedList
-from src.linked_list.reverse import reverse_linked_list
+from src.linked_list.reverse import reverse_linked_list, reverse_linked_list_rec
 from test.linked_list.utils import populate_linked_list_n_values
 
 
 def test_reverse_linked_list():
     linked_list = LinkedList()
     linked_list = populate_linked_list_n_values(linked_list, 5)
-    reversed_linked_list = reverse_linked_list(linked_list)
+    head_node = linked_list.head
+    reversed_head_node = reverse_linked_list(head_node)
+    reversed_list = LinkedList()
+    reversed_list.head = reversed_head_node
     expected_linked_status = "5 -> 4 -> 3 -> 2 -> 1"
-    assert expected_linked_status == reversed_linked_list.get_representation()
+    assert expected_linked_status == reversed_list.get_representation()
+
+def test_reverse_linked_list_rec():
+    linked_list = LinkedList()
+    linked_list = populate_linked_list_n_values(linked_list, 3)
+    head_node = linked_list.head
+    reversed_head_node = reverse_linked_list_rec(head_node)
+    reversed_list = LinkedList()
+    reversed_list.head = reversed_head_node
+    expected_linked_status = "3 -> 2 -> 1"
+    assert expected_linked_status == reversed_list.get_representation()
