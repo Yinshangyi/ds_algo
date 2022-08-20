ba024f09c7d739cd162a8a90b6f2b45767e5b8d7
Jean loic DE JAEGER (EXT) <jdejaeger.ext@galerieslafayette.com>
2022-08-20 16:15:46 +0200

    tree depth

---
+++ src/tree/tree_depth.py
@@ -0,0 +1,13 @@
+from src.tree.tree_node import TreeNode
+
+
+def get_depth_tree(root_node: TreeNode) -> int:
+    if root_node is None:
+        return -1
+
+    depth_left = get_depth_tree(root_node.left_child)
+    depth_right = get_depth_tree(root_node.right_child)
+
+    if depth_left > depth_right:
+        return depth_left + 1
+    return depth_right + 1
---
+++ test/tree/test_tree_depth.py
@@ -0,0 +1,9 @@
+from src.tree.tree_node import TreeNode
+from test.tree.test_tree_traversal import make_tree_nodes
+from src.tree.tree_depth import get_depth_tree
+
+
+def test_get_tree_depth():
+    root_node: TreeNode = make_tree_nodes()
+    tree_height = get_depth_tree(root_node)
+    assert tree_height == 2
